4b2f5a62b855ab895da6c241d8c5f88f12d7eb9c
NewBee <31762504+TassGab@users.noreply.github.com>
2017-10-03 13:02:42 +0200

    Add files via upload

---
+++ Example/CmdParser.ino
@@ -0,0 +1,16 @@
+#include "CmdParser.h"
+CmdParserClass CP=CmdParserClass();
+void setup() {
+  // put your setup code here, to run once
+  Serial.begin(9600);
+  String cmd="#1,5,55272.";
+  Serial.println(cmd);
+  CP.Parse(cmd);
+  Serial.print("error msg=");
+  Serial.println(CP.errMsg);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
